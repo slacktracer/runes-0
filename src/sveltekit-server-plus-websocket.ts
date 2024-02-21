5a16191ddf8e24234a5557e79399e44513c7b13a
slacktracer <slacktracer@gmail.com>
2024-02-20 23:16:12 -0300

    The production server...

---
+++ src/sveltekit-server-plus-websocket.ts
@@ -0,0 +1,12 @@
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+import { onHttpServerUpgrade } from './lib/server/websocket/on-http-server-upgrade.js';
+import { createWebsocketServerGlobalInstance } from './lib/server/websocket/create-websocket-server-global-instance.js';
+
+const directory = dirname(fileURLToPath(import.meta.url));
+
+createWebsocketServerGlobalInstance();
+
+const { server } = await import(resolve(directory, '../build/index.js'));
+
+server.server.on('upgrade', onHttpServerUpgrade);
