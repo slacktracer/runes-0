4442b334684c25f1c7b2edd213a4b700102315b1
slacktracer <slacktracer@gmail.com>
2024-02-22 20:45:17 -0300

    More types! Wow, this is hard!

--- src/lib/server/websocket/create-websocket-server-global-instance.ts
+++ src/lib/server/websocket/create-websocket-server-global-instance.ts
@@ -1,28 +1,14 @@
 import { symbolForWebsocketServer } from './symbol-for-websocket-server.js';
-import { WebSocketServer } from 'ws';
-import type { IncomingMessage } from 'http';
-import type { Duplex } from 'stream';
+import { type WebSocket, WebSocketServer } from 'ws';
+import type { GlobalThisPlusWebSocketServer } from '../../../types/GlobalThisPlusWebSocketServer.js';
 
 export const createWebsocketServerGlobalInstance = () => {
 	const webSocketServer = new WebSocketServer({ noServer: true });
 
-	(
-		globalThis as typeof globalThis & {
-			[symbolForWebsocketServer]: {
-				emit: (
-					eventName: string,
-					webSocket: unknown,
-					request: IncomingMessage
-				) => void;
-				handleUpgrade: (
-					request: IncomingMessage,
-					socket: Duplex,
-					upgradeHead: Buffer,
-					callback: (websocket: unknown) => void
-				) => void;
-			};
-		}
-	)[symbolForWebsocketServer] = webSocketServer;
+	(globalThis as GlobalThisPlusWebSocketServer)[symbolForWebsocketServer] =
+		webSocketServer as WebSocketServer & {
+			clients: Set<WebSocket & { socketID: string }>;
+		};
 
 	return webSocketServer;
 };
