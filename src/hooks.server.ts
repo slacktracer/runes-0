f10b6da5286e86426c01ca40a6458b73f649a12b
slacktracer <slacktracer@gmail.com>
2024-02-23 21:18:43 -0300

    If I can use the exported websocket server I see no reason to add it to server locals.

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,22 +1,8 @@
 import type { Handle } from '@sveltejs/kit';
-import { building } from '$app/environment';
 import { startWebSocketServer } from './lib/server/web-socket/start-web-socket-server.js';
-import { symbolForWebSocketServer } from './lib/server/web-socket/symbol-for-web-socket-server.js';
-import type { GlobalPlusWebSocketServer } from './lib/server/web-socket/types/GlobalPlusWebSocketServer.js';
 
 export const handle = (async ({ event, resolve }) => {
 	startWebSocketServer();
-
-	if (!building) {
-		const websocketServer = (global as GlobalPlusWebSocketServer)[
-			symbolForWebSocketServer
-		];
-
-		if (websocketServer !== undefined) {
-			// @ts-expect-error Got to find out how to extend event.locals type. But maybe I do not need it for this project.
-			event.locals.websocketServer = websocketServer;
-		}
-	}
 
 	const response = await resolve(event, {
 		filterSerializedResponseHeaders: (name) => name === 'content-type'
