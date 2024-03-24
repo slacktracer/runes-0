02f79979753a04cab05532d68669ee1b4cac4d68
slacktracer <slacktracer@gmail.com>
2024-03-24 12:23:54 -0300

    Renaming the Tablet directory.

--- src/routes/play/+page.svelte
+++ src/routes/play/+page.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
+  import ASide from "../../lib/runes/components/ASide/ASide.svelte";
   import BSide from "../../lib/runes/components/BSide/BSide.svelte";
-  import Tablet from "../../lib/runes/components/Tablet/ASide.svelte";
   import Tabula from "../../lib/runes/components/Tabula.svelte";
 </script>
 
 <Tabula>
-  <Tablet slot="a-side"></Tablet>
+  <ASide slot="a-side"></ASide>
   <BSide slot="b-side"></BSide>
 </Tabula>
